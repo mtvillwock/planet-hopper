71d3d7c1cfdaa52ce87780d022e3800efa8fe198
Matthew Villwock <mtvillwock@gmail.com>
2015-04-19 23:18:39 -0700

    write create route for new trip form to hit, successful return json

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,6 +10,21 @@ class TripsController < ApplicationController
   end
 
   def create
+    @trip = Trip.new(
+      destination_country: params[:trip][:destination_country],
+      destination_city: params[:trip][:destination_city],
+      start_date: params[:trip][:start_date],
+      end_date: params[:trip][:end_date],
+      origin_country: params[:trip][:origin_country],
+      origin_city: params[:trip][:origin_city])
+    # Revise based on link below:
+    # http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+    # @trip = Trip.new(params[:trip])
+    if @trip.save
+      render json: { success: "trip created" }
+    else
+      render json: { error: "trip failed to create" }
+    end
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420043520) do
+ActiveRecord::Schema.define(version: 20150420052038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20150420043520) do
     t.datetime "return_date"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "passports", force: :cascade do |t|
+    t.string   "origin_country"
+    t.string   "destination_country"
+    t.integer  "user_id"
+    t.integer  "country_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   create_table "trips", force: :cascade do |t|
