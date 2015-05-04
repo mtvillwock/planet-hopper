eeaf8e5e7afecfff678be673b1e8c441bdf05b59
Matthew Villwock <mtvillwock@gmail.com>
2015-05-03 22:22:03 -0700

    add duration to trips migration, draft simple version of new trip form based on PDF mockup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @trip = Trip.new
   end
 
   def show
--- db/migrate/20150420042650_create_trips.rb
+++ db/migrate/20150420042650_create_trips.rb
@@ -3,7 +3,6 @@ class CreateTrips < ActiveRecord::Migration
     create_table :trips do |t|
       t.date :start_date
       t.date :end_date
-      # add duration
       t.string :destination_country
       t.string :destination_city
       t.string :origin_country
