1962fe736d313b5ba4c69ea212e4b8b176b79427
Matthew Villwock <mtvillwock@gmail.com>
2015-06-08 19:51:52 -0700

    add logic for user controller  routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,42 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    # @user = User.find(params[:username])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save!
+      redirect_to '/login'
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+    # @user = User.find(params[:username])
   end
 
   def update
+    @user = User.find(params[:id])
+    # @user = User.find(params[:username])
+    if @user.update(user_params)
+      redirect_to 'show'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    # @user = User.find(params[:username])
+    @user.destroy
+    redirect_to '/'
   end
 end
