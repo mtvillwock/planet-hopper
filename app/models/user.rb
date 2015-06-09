8d2cc76146c8d275e72800ec59a68e945acabe51
Matthew Villwock <mtvillwock@gmail.com>
2015-06-08 20:21:46 -0700

    add Bcrypt, password_hash, edit user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
   has_many :trips
   has_many :passports
+
+  validates_presence_of :email, :password_hash
+  validates_uniqueness_of :email, :password_hash
+
+  private
+  ## Bcrypt
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
