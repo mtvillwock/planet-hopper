4c51ba7efb4b988cc45314348871c29589778b6a
Matthew Villwock <mtvillwock@gmail.com>
2015-06-13 19:04:25 -0700

    attempting controller testing for users; WIP

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'factory_girl_rails'
 
 RSpec.describe UsersController, :type => :controller do
 
@@ -7,11 +8,24 @@ RSpec.describe UsersController, :type => :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
   describe "GET show" do
-    it "returns http success" do
-      get :show
+    let(:user) do
+      # User.all.destroy_all
+      user = User.create(email: Faker::Internet.email)
+      user.password = Faker::Internet.password
+      user.save
+      user
+    end
+    let(:id) { session[:user_id] = user.id }
+    let(:user_id) { controller.send(:current_user).id }
+    xit "returns http success" do
+      get :show, id: user_id
       expect(response).to have_http_status(:success)
     end
   end
@@ -20,13 +34,18 @@ RSpec.describe UsersController, :type => :controller do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
+      expect(response).to render_template :new
     end
   end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST create" do
+    context 'with valid attributes' do
+      it 'saves the new user in the database' do
+        user = User.new(email: Faker::Internet.email)
+        expect{
+          post '/users', user
+        }.to change(User, :count).by(1)
+      end
     end
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,18 @@
 
 FactoryGirl.define do
   factory :user do
+    factory :valid_user do
+      email Faker::Internet.email
+      password Faker::Internet.password
+      before(:save) { self.password = password }
+    end
+    factory :invalid_user do
+      factory :user_without_email do
+        email nil
+      end
+      factory :user_without_password do
+        email "joe@example.com"
+      end
+    end
   end
 end
