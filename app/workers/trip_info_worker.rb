cbcf9f8d2abba62e405783dba661504b21cc54b3
Matthew Villwock <mtvillwock@gmail.com>
2015-06-15 22:21:06 -0700

    able to hit worker.perform; need to write API call

--- app/workers/trip_info_worker.rb
+++ app/workers/trip_info_worker.rb
@@ -1,9 +1,10 @@
 class TripInfoWorker
   include Sidekiq::Worker
 
-  def perform(trip_attributes)
-    trip_attributes = JSON.load(trip_attributes)
-    puts "trip_attributes are: #{trip_attributes}"
+  def perform(trip_id)
+    trip = Trip.find(trip_id)
+    p "The trip is "
+    p trip
     # do all the tasks required to compile trip info
   end
 end
