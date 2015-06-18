12ab3fdd6bff83ee0d4b5d9c6837a804183d7c35
Matthew Villwock <mtvillwock@gmail.com>
2015-06-17 22:24:54 -0700

    temp changes to get airport codes to populate origin/destination, show start/end

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -19,10 +19,13 @@ class TripsController < ApplicationController
 
   def create
     user = current_user
+    p trip_params
     @trip = Trip.new(trip_params)
     if @trip.save
       user.trips << @trip
+      p @trip.attributes
       TripInfoWorker.new.perform(@trip.id)
+      FlightSearchWorker.new.perform(trip_params)
       redirect_to @trip
     else
       render 'new'
@@ -51,31 +54,6 @@ class TripsController < ApplicationController
     end
   end
 
-  def get_cities
-    # fetch all cities and compare possible matches to input from AJAX call
-    # return list of cities that match based on comparing N letters
-    # received from AJAX call with N letters of each city in cities array
-    # City model/migration could be useful but for
-    # now I am spoofing it with a sample array
-    city_options = []
-    possible_cities = ["London", "Tokyo", "San Francisco", "New York", "Madison", "Milwaukee", "Boston", "Los Angeles", "Toronto", "San Diego", "Santa Clara", "Green Bay"]
-    # maybe a hash with city name as a string and city object as a value
-    p "form input is: "
-    p params[:city_input]
-    # Switch this in JS and here to destination_city
-    # Make params match up with form input names
-    possible_cities.each do |city|
-      p "city is: "
-      p city
-      p "portion of option city minus input: "
-      p city[0..params[:city_input].length - 1]
-      city_options << city if city[0..params[:city_input].length - 1].downcase == params[:city_input].downcase
-    end
-    p "the options are: "
-    p city_options
-    render json: { city_options: city_options }
-  end
-
   def departure_airports
     # fetch all airports, compare possible matches to input from AJAX call
     # City model/migration could be useful, spoofing it with a sample array
@@ -96,6 +74,46 @@ class TripsController < ApplicationController
     p "the options are: "
     p airport_options
     render json: { airport_options: airport_options }
+  end
+
+  def get_cities
+    # fetch all airports, compare possible matches to input from AJAX call
+    # City model/migration could be useful, spoofing it with a sample array
+    airport_options = []
+    possible_airports = ["SFO", "JFK", "SEA-TAC", "DFW", "SBA", "SBP", "SBN"]
+    # maybe a hash with city name as a string and city object as a value
+    p "form input is: "
+    p params[:city_input]
+    # Switch this in JS and here
+    # Make params match up with form input names
+    possible_airports.each do |airport|
+      airport_options << airport if airport[0..params[:city_input].length - 1].downcase == params[:city_input].downcase
+    end
+    p "the options are: "
+    p airport_options
+    render json: { city_options: airport_options }
+    # fetch all cities and compare possible matches to input from AJAX call
+    # return list of cities that match based on comparing N letters
+    # received from AJAX call with N letters of each city in cities array
+    # City model/migration could be useful but for
+    # now I am spoofing it with a sample array
+    # city_options = []
+    # possible_cities = ["London", "Tokyo", "San Francisco", "New York", "Madison", "Milwaukee", "Boston", "Los Angeles", "Toronto", "San Diego", "Santa Clara", "Green Bay"]
+    # # maybe a hash with city name as a string and city object as a value
+    # p "form input is: "
+    # p params[:city_input]
+    # # Switch this in JS and here to destination_city
+    # # Make params match up with form input names
+    # possible_cities.each do |city|
+    #   p "city is: "
+    #   p city
+    #   p "portion of option city minus input: "
+    #   p city[0..params[:city_input].length - 1]
+    #   city_options << city if city[0..params[:city_input].length - 1].downcase == params[:city_input].downcase
+    # end
+    # p "the options are: "
+    # p city_options
+    # render json: { city_options: city_options }
   end
 
   def timespans
--- app/workers/flight_search_worker.rb
+++ app/workers/flight_search_worker.rb
@@ -1,14 +1,31 @@
-require 'httparty'
+require 'typhoeus'
+
 class FlightSearchWorker
-  include HTTParty
 
-  def perform
-    key = ENV["GOOGLE_API_KEY"]
-
-    flight = Flight.new
+  def perform(args)
+    p args
+    p airport = Airport.where(airport_code: args[:destination_city]).first!
     # make request to get flight and parse response
-    request = HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?prettyPrint=true&key=#{key}")
-    p "The flight is "
-    p flight
+    # include another slice for roundtrip
+   p  request = {
+      "request" => {
+        "slice" => [
+          {
+            "origin" => args[:origin_city],
+            "destination" => airport,
+            "date" => args[:start_date]
+          },
+        ],
+        "passengers" => {
+          "adultCount" => 1
+        },
+        "solutions" => 1,
+        "refundable" => false
+      }
+    }.to_json
+    # will need to change number of solutions if using this as background job
+    # that way can make many requests using Hydra without having client wait 10+ seconds
+
+    p request_hydra = Typhoeus::Request.new("https://www.googleapis.com/qpxExpress/v1/trips/search?prettyPrint=true&key=#{ENV['GOOGLE_API_KEY']}", method: :post, body: request, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }, followlocation: true)
   end
 end
