379cc82d723a47bd8e154aaa9314a852d4cb1e93
Matthew Villwock <mtvillwock@gmail.com>
2015-06-20 22:09:24 -0700

    reduce flight requests to 1

--- app/workers/flight_search_worker.rb
+++ app/workers/flight_search_worker.rb
@@ -27,14 +27,14 @@ class FlightSearchWorker
         "passengers" => {
           "adultCount" => 1
         },
-        "solutions" => 10,
+        "solutions" => 1,
         "refundable" => false
       }
     }.to_json
     # will need to change number of solutions if using this as background job
     # that way can make many requests using Hydra without having client wait 10+ seconds
     @request_array = []
-    p @hydra = Typhoeus::Hydra.new
+    @hydra = Typhoeus::Hydra.new
     p request_hydra = Typhoeus::Request.new("https://www.googleapis.com/qpxExpress/v1/trips/search?prettyPrint=true&key=#{ENV['GOOGLE_API_KEY']}", method: :post, body: request, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }, followlocation: true)
     @request_array << request_hydra
     @hydra.queue(request_hydra)
